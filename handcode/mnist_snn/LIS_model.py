91e579fa6f0bf22e5467122657f053f90cf0ba1c
wangzhenCS <130069028+wangzhenCS@users.noreply.github.com>
2023-08-09 15:09:14 +0800

    Update LIS_model.py
    change

--- handcode/mnist_snn/LIS_model.py
+++ handcode/mnist_snn/LIS_model.py
@@ -43,8 +43,8 @@ class LIS_model(nn.Module):
         self.v_threshold = 0.2
         self.v_reset = None
 
-        self.cnn = ((2, 64, 3, 1, 1, 5, 2), (64, 64, 3, 1, 1, 5, 2))
-        self.kernel = (36, 18, 9)
+        self.cnn = ((1, 32, 3, 1, 1, 5, 2), (32, 32, 3, 1, 1, 5, 2))
+        self.kernel = (28, 14, 7)
 
         self.conv1 = nn.Conv2d(self.cnn[0][0], self.cnn[0][1], kernel_size = self.cnn[0][2], stride = self.cnn[0][3], padding = self.cnn[0][4], bias = if_bias)
         self.conv2 = nn.Conv2d(self.cnn[1][0], self.cnn[1][1], kernel_size = self.cnn[1][2], stride = self.cnn[1][3], padding = self.cnn[1][4], bias = if_bias)
